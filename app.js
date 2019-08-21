0634d23e9309965c21527a6fd1f941599aab4765
Joshua <mama27j@gmail.com>
2019-08-21 12:16:22 +0300

    add cors support

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 app.use('/faas/function', indexRouter);
 app.use('/faas/manage', manageRouter);
 
