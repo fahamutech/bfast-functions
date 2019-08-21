a3df244f6a3ce667b4cb333f95100b36a9c5ad48
Joshua <mama27j@gmail.com>
2019-08-21 13:35:15 +0300

    update request limit

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const app = express();
 app.use(cors());
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({
+    limit: '2024mb'
+}));
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
