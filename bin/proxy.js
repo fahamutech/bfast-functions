af701d7a7ca939459e05a155736b93f3d645dbda
joshua <mama27j@gmail.com>
2019-10-12 12:23:06 +0300

    chnage release to alpha and add logger to proxy

--- bin/proxy.js
+++ bin/proxy.js
@@ -2,7 +2,7 @@
 const http = require('http');
 const express = require('express');
 const cookieParser = require('cookie-parser');
-// const logger = require('morgan');
+const logger = require('morgan');
 const cors = require('cors');
 const app = express();
 const path = require('path');
@@ -20,7 +20,7 @@ const projectId  = process.env.PROJECT_ID;
 let faasForkPid = undefined;
 
 app.use(cors());
-// app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json({
     limit: '2024mb'
 }));
