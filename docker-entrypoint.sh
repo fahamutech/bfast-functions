7920c494aaccce792a18bcd13a9bf27bf8f47009
joshua <mama27j@gmail.com>
2021-09-06 11:05:41 +0200

    use go ipfs node instead of js-ipfs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,4 @@ set -e
 
 exec jsipfs init
 exec jsipfs config --json Addresses.API '["/ip4/127.0.0.1/tcp/5002"]'
-exec jsipfs daemon &
-exec npm run start "$@"
+exec jsipfs daemon & npm run start "$@"
